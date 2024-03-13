8c01e75b99a02f50a5daac30b3e0208b7d23a107
leidyolinto <suverleide.olinto@gmail.com>
2024-03-13 12:24:29 -0300

    Test da SessionService

---
+++ test/sessionService/session-service.test.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+const SessionService = require('../../src/services/session-service');
+require('dotenv').config(); 
+
+describe('SessionService', () => {
+  test('should generate a valid token', () => {
+    const secretKey = process.env.SECRET_KEY || 'default_secret_key'; 
+    const email = 'equipe.ada@gmail.com';
+    const token = SessionService.generateToken({ email });
+    const decodedToken = jwt.verify(token, secretKey);
+    expect(decodedToken.email).toBe(email);
+  });
+
+  test('should throw an error if SECRET_KEY is not provided', () => {
+    const originalSecretKey = process.env.SECRET_KEY;
+    delete process.env.SECRET_KEY; 
+
+    const email = 'equipe.ada@gmail.com';
+
+    expect(() => SessionService.generateToken({ email })).toThrow('JsonWebTokenError');
+
+    process.env.SECRET_KEY = originalSecretKey;
+  });
+});
