bb2d04547ea2a99896b2becfb27c4b5e7747f650
leidyolinto <suverleide.olinto@gmail.com>
2024-03-14 11:33:27 -0300

    Alterando o test should throw an error if SECRET_KEY is not provided

--- test/sessionService/session-service.test.js
+++ test/sessionService/session-service.test.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const SessionService = require('../../src/services/session-service');
+
 require('dotenv').config(); 
 
 describe('SessionService', () => {
@@ -10,15 +11,17 @@ describe('SessionService', () => {
     const decodedToken = jwt.verify(token, secretKey);
     expect(decodedToken.email).toBe(email);
   });
-
   test('should throw an error if SECRET_KEY is not provided', () => {
     const originalSecretKey = process.env.SECRET_KEY;
-    delete process.env.SECRET_KEY; 
-
+    delete process.env.SECRET_KEY;
+  
     const email = 'equipe.ada@gmail.com';
-
-    expect(() => SessionService.generateToken({ email })).toThrow('JsonWebTokenError');
-
+  
+    expect(() => {
+      SessionService.generateToken({ email });
+    }).toThrow('secretOrPrivateKey must have a value');
+  
     process.env.SECRET_KEY = originalSecretKey;
   });
+
 });
